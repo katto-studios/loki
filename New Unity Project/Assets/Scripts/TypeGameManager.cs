ceae00f63da21c81352a7934cc8696f04ca6c9b2
ryanntannn <tanryan777@gmail.com>
2020-02-21 17:04:57 +0800

    Add TypeInputManager and TypeGameManager

---
+++ New Unity Project/Assets/Scripts/TypeGameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TypeGameManager : MonoBehaviour
+{
+
+    string inputString = "";
+    string inputWord = "";
+    public TextMeshProUGUI inputTextMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateTextMesh()
+    {
+        inputTextMesh.text = inputWord;
+    }
+
+    public void AddCharacterToInputString(char character)
+    {
+        inputString += character;
+        inputWord += character;
+
+        if(character == ' ')
+        {
+            inputWord = "";
+        }
+
+        UpdateTextMesh();
+    }
+
+    public void BackSpacePressed()
+    {
+        if(inputWord.Length != 0)
+        {
+            inputString = inputString.Substring(0, inputString.Length - 1);
+            inputWord = inputWord.Substring(0, inputWord.Length - 1);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/TypeInputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeInputManager : MonoBehaviour
+{
+    public TypeGameManager typeGameManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LetterCheck();
+    }
+
+    void LetterCheck()
+    {
+        foreach (char c in Input.inputString)
+        {
+            if (c != "\b"[0]) //Handle Backspace;
+            {
+                typeGameManager.BackSpacePressed();
+            }
+            else
+            {
+                Debug.Log(c);
+                typeGameManager.AddCharacterToInputString(c);
+            }
+        }
+    }
+}
