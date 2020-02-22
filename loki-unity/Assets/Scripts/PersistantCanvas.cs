b4e6d80f8d1285220951f4e46846a39c6cc47dd7
ryanntannn <tanryan777@gmail.com>
2020-02-23 00:34:55 +0800

    Add Persistant Canvas

---
+++ loki-unity/Assets/Scripts/PersistantCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantCanvas : Singleton<PersistantCanvas>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
