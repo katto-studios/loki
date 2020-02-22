e856768ac704f48ee87774f28606d85bf5610fab
Depressionn <tantiongguan123@gmail.com>
2020-02-23 01:41:22 +0800

    Tested some stuff

---
+++ loki-unity/Assets/Scripts/Playfab/GetProse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.Json;
+using PlayFab.ClientModels;
+
+public class GetProse{
+    public static string GetRandomProse() {
+        return null;
+    }
+}
