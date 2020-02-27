979ee98f6256bc2c718190bd511194c63b6855a8
Depressionn <tantiongguan123@gmail.com>
2020-02-27 23:52:09 +0800

    Ready for testing

---
+++ loki-unity/Assets/Scripts/Multiplayer/WhackMultiplayer/MultiplayerClient.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using UnityEngine;
+using System.Text;
+using System;
+using UnityEngine.UI;
+
+public class MultiplayerClient {
+    private string m_consoleTxt;
+    public string ConsoleTxt { get { return m_consoleTxt; } }
+    //private Thread m_clientThread;
+    private string m_ip = "127.0.0.1";
+    private TcpClient m_client = null;
+
+    public MultiplayerClient(string _ip = "127.0.0.1") {
+        m_ip = _ip;
+
+        ConnectToServer();
+    }
+
+    private void ConnectToServer() {
+        //create tcp client
+        try {
+            m_client = new TcpClient(m_ip, 42069);
+
+            byte[] data = Encoding.ASCII.GetBytes("Client connection request");
+            NetworkStream stream = m_client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+            Debug.Log(PlayfabUserInfo.GetUsername() + ": Client connection request");
+            m_consoleTxt += "\n" + PlayfabUserInfo.GetUsername() + ": Client connection request";
+
+            data = new byte[256];
+
+            int bytes = stream.Read(data, 0, data.Length);
+            string responseData = Encoding.ASCII.GetString(data, 0, bytes);
+            Debug.Log("Recieved: " + responseData);
+            m_consoleTxt += "\nRecieved: " + responseData;
+        } catch (Exception e) {
+            Debug.Log(e.Message);
+            m_consoleTxt += "\nError: " + e.Message;
+        }
+    }
+
+    public void SendMessageToServer(string _message) {
+        try {
+            NetworkStream stream = m_client.GetStream();
+
+            byte[] sendToServer = Encoding.ASCII.GetBytes(_message);
+            Debug.Log(PlayfabUserInfo.GetUsername() + _message);
+            m_consoleTxt += "\n" + PlayfabUserInfo.GetUsername() + _message;
+            stream.Write(sendToServer, 0, sendToServer.Length);
+        } catch (Exception e) {
+            Debug.Log(e.Message);
+            m_consoleTxt += "\nError: " + e.Message;
+        }
+    }
+
+    public void StopClient() {
+        m_client.GetStream().Close();
+        m_client.Close();
+    }
+
+    ~MultiplayerClient() {
+        StopClient();
+    }
+}
---
+++ loki-unity/Assets/Scripts/Multiplayer/WhackMultiplayer/MultiplayerServer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using UnityEngine;
+using System.Text;
+using System;
+using UnityEngine.UI;
+
+public class MultiplayerServer {
+    private string m_consoleTxt;
+    public string ConsoleTxt { get { return m_consoleTxt; } }
+    private volatile bool m_shuttingDown = false;
+    private Thread m_serverThread;
+    private string m_ip = "127.0.0.1";
+    private TcpListener m_server;
+
+    public MultiplayerServer(string _ip = "127.0.0.1") {
+        m_ip = _ip;
+
+        m_serverThread = new Thread(CreateServer);
+        m_serverThread.Start();
+    }
+
+    private void CreateServer() {
+        try {
+            m_server = new TcpListener(IPAddress.Parse(m_ip), 42069);
+            m_server.Start();
+
+            while (true) {
+                UpdateLoop();
+            }
+        } catch (Exception e) {
+            Debug.Log(e.Message);
+            m_consoleTxt += "\nError: " + e.Message;
+        }
+    }
+
+    private void UpdateLoop() {
+        try {
+            byte[] buffer = new byte[256];
+            string data;
+
+            Debug.Log("Waiting for connection");
+            m_consoleTxt += "\nWaiting for connection";
+
+            TcpClient client = m_server.AcceptTcpClient();
+            Debug.Log("Client connected!");
+            m_consoleTxt += "\nClient connected";
+            NetworkStream stream = client.GetStream();
+
+            int count;
+            while ((count = stream.Read(buffer, 0, buffer.Length)) != 0) {
+                data = Encoding.ASCII.GetString(buffer, 0, count);
+                Debug.Log("Server recieved: " + data);
+                m_consoleTxt += "\nServer recieved: " + data;
+
+                data = "Ack";
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+                Debug.Log( "Server: " + data);
+                m_consoleTxt += "\nServer: " + data;
+            }
+            Thread.Sleep(1);
+
+            client.Close();
+        }catch(Exception e) {
+            Debug.Log(e.Message);
+            m_consoleTxt += "\n" + e.Message;
+        }
+    }
+
+    public void StopServer() {
+        m_shuttingDown = true;
+        m_serverThread.Abort();
+    }
+
+    ~MultiplayerServer() {
+        StopServer();
+    }
+}
