4d0ed454bc7c93cde708110967a6bee3189b74ca
ryanntannn <tanryan777@gmail.com>
2020-03-09 18:10:12 +0800

    Add Artisan Keycap Scriptable Object

---
+++ loki-unity/Assets/Scripts/Keyboard/ArtisanKeycap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum KeycapRarity {
+    COMMON,
+    RARE,
+    EPIC,
+    LEGENDARY,
+    UNIQUE
+};
+
+[CreateAssetMenu]
+public class ArtisanKeycap : ScriptableObject
+{
+    public string name;
+    public int id;
+    public GameObject keycap;
+    public KeycapRarity rarity;
+}
