0c3d1e828f014a69b86d8cedb51f1f2d3def83e8
ryanntannn <tanryan777@gmail.com>
2020-03-11 16:23:01 +0800

    Add KeycapDatabase Script

---
+++ loki-unity/Assets/Scripts/Keyboard/KeycapDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeycapDatabase : Singleton<KeycapDatabase>
+{
+    [SerializeField]
+    private List<ArtisanKeycap> artisanKeycaps;
+
+    void Start()
+    {
+        if (FindObjectsOfType<KeycapDatabase>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public ArtisanKeycap getKeyCapFromID(int id)
+    {
+        foreach(ArtisanKeycap ak in artisanKeycaps)
+        {
+            if(id == ak.id)
+            {
+                return ak;
+            }
+        }
+        Debug.Log("Keycap " + id + " does not exist");
+        return null; //Error
+    }
+}
