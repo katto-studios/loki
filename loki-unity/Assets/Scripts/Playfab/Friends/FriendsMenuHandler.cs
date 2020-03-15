acab75665d4bc17077d312da381586cccb85e947
Depressionn <tantiongguan123@gmail.com>
2020-03-15 23:34:38 +0800

    Basic 2way friendship added

--- loki-unity/Assets/Scripts/Playfab/Friends/FriendsMenuHandler.cs
+++ loki-unity/Assets/Scripts/Playfab/Friends/FriendsMenuHandler.cs
@@ -11,22 +11,39 @@ public class FriendsMenuHandler : MonoBehaviour {
 	public GameObject friendInfoPfb;
 	[Header("Add friend")]
 	public InputField inSearch;
+    [Header("Pending")]
+    public GameObject pendingFriends;
+    public GameObject pendingFriendPfb;
 
-	private List<cm::FriendInfo> m_friends = new List<cm.FriendInfo>();
+    private List<cm::FriendInfo> m_friends = new List<cm.FriendInfo>();
+    private List<cm::FriendInfo> m_pending = new List<cm.FriendInfo>();
 
-	private void Awake() {
+    private void Awake() {
 		PlayfabUserInfo.GetFriendsList(this);
 	}
 
-	public void AddToList(cm::FriendInfo _newFriend) {
+	public void AddToFriendsList(cm::FriendInfo _newFriend) {
 		m_friends.Add(_newFriend);
 		FriendDisplay instanceDisplay = Instantiate(friendInfoPfb, friendContent.transform).GetComponent<FriendDisplay>();
 		instanceDisplay.SetFriendInfo(_newFriend);
 	}
+
+    public void AddToPendingFriendsList(cm::FriendInfo _pending) {
+        m_pending.Add(_pending);
+        PendingDisplay instanceDisplay = Instantiate(pendingFriendPfb, pendingFriends.transform).GetComponent<PendingDisplay>();
+        instanceDisplay.Initalise(_pending.TitleDisplayName);
+    }
 
 	public void AddFriend() {
 		PlayfabUserInfo.AddFriend(inSearch.text);
 		//refresh list
 		PlayfabUserInfo.GetFriendsList(this);
 	}
+
+    //listener for button
+    public void AcceptFriend() {
+        PlayfabUserInfo.AcceptFriend(GetComponent<FriendDisplay>().nameDisplay.text);
+        //refresh list
+        PlayfabUserInfo.GetFriendsList(this);
+    }
 }
--- loki-unity/Assets/Scripts/Playfab/PlayfabUserInfo.cs
+++ loki-unity/Assets/Scripts/Playfab/PlayfabUserInfo.cs
@@ -11,6 +11,17 @@ public static class PlayfabUserInfo {
     private static UserAccountInfo m_accountInfo;
     public static UserAccountInfo AccountInfo { get { return m_accountInfo; } }
     public static List<ArtisanKeycap> playerKeycaps;
+    private static List<cm::FriendInfo> m_friends;
+    public static List<cm::FriendInfo> Friends { get { return m_friends; } }
+
+    public enum UserState {
+        InMainMenu,
+        ReadyToPractice, Practicing, ViewingLeaderBoards,
+        Disconnected, InLobby, InQueue, ReadyToType,
+        WaitingForOpponent, InMatch, WaitingForNextRound
+    }
+    private static UserState m_userState = UserState.Disconnected;
+    public static UserState CurrentUserState { get { return m_userState; } }
 
     public static void Initalise() {
         GetAccountInfoRequest req = new GetAccountInfoRequest();
@@ -158,45 +169,6 @@ public static class PlayfabUserInfo {
         );
         return newInventory;
     }
-	private static UserAccountInfo m_accountInfo;
-	public static UserAccountInfo AccountInfo { get { return m_accountInfo; } }
-	private static List<cm::FriendInfo> m_friends;
-	public static List<cm::FriendInfo> Friends { get { return m_friends; } }
-
-	public enum UserState {
-		InMainMenu,
-		ReadyToPractice, Practicing, ViewingLeaderBoards,
-		Disconnected, InLobby, InQueue, ReadyToType,
-		WaitingForOpponent, InMatch, WaitingForNextRound
-	}
-	private static UserState m_userState = UserState.Disconnected;
-	public static UserState CurrentUserState { get { return m_userState; } }
-
-	public static void Initalise() {
-		GetAccountInfoRequest req = new GetAccountInfoRequest();
-		PlayFabClientAPI.GetAccountInfo(
-			req,
-			(_result) => { m_accountInfo = _result.AccountInfo; },
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-
-		PlayFabClientAPI.GetFriendsList(
-			new GetFriendsListRequest()
-			{
-				IncludeFacebookFriends = false,
-				IncludeSteamFriends = false,
-				XboxToken = null
-			},
-			(_results) => {
-				m_friends = _results.Friends;
-			},
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-
-		SetUserState(UserState.InMainMenu);
-
-		PersistantCanvas.Instance.StartCoroutine(SetDisplayName());
-	}
 
 	public static void SetUserState(UserState _newState) {
 		m_userState = _newState;
@@ -216,73 +188,6 @@ public static class PlayfabUserInfo {
 		Debug.Log(string.Format("New user state: {0}", m_userState));
 	}
 
-	private static IEnumerator SetDisplayName() {
-		while (GetUsername().Equals("")) {
-			yield return null;
-		}
-
-		PlayFabClientAPI.UpdateUserTitleDisplayName(
-			new UpdateUserTitleDisplayNameRequest() { DisplayName = GetUsername() },
-			(_result) => { },
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-	}
-
-	public static string GetUsername() {
-		if (m_accountInfo != null) {
-			return m_accountInfo.Username;
-		}
-		return string.Empty;
-	}
-
-	public static void UpdateWpm(int _totalWords, float _secondsSinceStart) {
-		PersistantCanvas.Instance.StartCoroutine(SetWpm(_totalWords, (int)_secondsSinceStart));
-	}
-
-	private static IEnumerator SetWpm(int _totalWords, int _time) {
-		bool done = false;
-
-		PlayFabClientAPI.ExecuteCloudScript(
-			new ExecuteCloudScriptRequest()
-			{
-				FunctionName = "UpdatePlayerWpm",
-				FunctionParameter = new { TotalWords = _totalWords, TotalTime = _time },
-			},
-			(_result) => {
-				Debug.Log("Done updating wpm");
-				done = true;
-			},
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-
-		while (!done) {
-			yield return null;
-		}
-	}
-
-	public static void UpdateHighscore(int _newScore) {
-		PlayFabClientAPI.ExecuteCloudScript(
-			new ExecuteCloudScriptRequest()
-			{
-				FunctionName = "UpdatePlayerHighScore",
-				FunctionParameter = new { Points = _newScore },
-			},
-			(_result) => { },
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-	}
-
-	public static void UpdateTotalGames() {
-		PlayFabClientAPI.ExecuteCloudScript(
-			new ExecuteCloudScriptRequest()
-			{
-				FunctionName = "UpdateTotalGames",
-			},
-			(_result) => { },
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-	}
-
 	public static void UpdatePlayerMmr(int _mmr) {
 		PlayFabClientAPI.ExecuteCloudScript(
 			new ExecuteCloudScriptRequest()
@@ -295,27 +200,8 @@ public static class PlayfabUserInfo {
 		);
 	}
 
-	//doesnt work btw
-	public static int GetPlayerStatistic(string _stat) {
-		int returnThis = -1;
-
-		PlayFabClientAPI.GetPlayerStatistics(
-			new GetPlayerStatisticsRequest(),
-			(_result) => {
-				foreach (StatisticValue stat in _result.Statistics) {
-					if (stat.StatisticName.Equals(_stat)) {
-						returnThis = stat.Value;
-						break;
-					}
-				}
-			},
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
-
-		return returnThis;
-	}
-
-	public static void GetFriendsList(FriendsMenuHandler _handle) {
+    #region Friends
+    public static void GetFriendsList(FriendsMenuHandler _handle) {
 		PlayFabClientAPI.GetFriendsList(
 			new GetFriendsListRequest(){
 				IncludeFacebookFriends = false,
@@ -323,7 +209,11 @@ public static class PlayfabUserInfo {
 			},
 			(_result) => {
 				foreach (cm::FriendInfo friend in _result.Friends) {
-					_handle.AddToList(friend);
+                    if (friend.Tags.Contains("Friends")) {
+                        _handle.AddToFriendsList(friend);
+                    }else if (friend.Tags.Contains("Requestee")) {
+                        _handle.AddToPendingFriendsList(friend);
+                    }
 				}
 			},
 			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
@@ -331,14 +221,49 @@ public static class PlayfabUserInfo {
 	}
 
 	public static void AddFriend(string _friendName) {
-		PlayFabClientAPI.ExecuteCloudScript(
-			new ExecuteCloudScriptRequest()
-			{
-				FunctionName = "AddFriend",
-				FunctionParameter = new { RecieverName = _friendName },
-			},
-			(_result) => { },
-			(_error) => { Debug.LogError(_error.GenerateErrorReport()); }
-		);
+        //get friends id
+        PlayFabClientAPI.GetAccountInfo(
+            new GetAccountInfoRequest() {
+                Username = _friendName
+            },
+            (_result) => {
+                //actually add the friend
+                PlayFabClientAPI.ExecuteCloudScript(
+                    new ExecuteCloudScriptRequest() {
+                        FunctionName = "AddFriend",
+                        FunctionParameter = new { ReceiverId = _result.AccountInfo.PlayFabId },
+                    },
+                    (__result) => { Debug.Log(string.Format("Friend request sent to {0}", _friendName)); },
+                    (__error) => { Debug.LogError(__error.GenerateErrorReport()); }
+                );
+            },
+            (_error) => { Debug.LogError(_error.GenerateErrorReport()); }
+        );
 	}
+
+    public static void AcceptFriend(string _friendName) {
+        //get friends id
+        PlayFabClientAPI.GetAccountInfo(
+            new GetAccountInfoRequest() {
+                Username = _friendName
+            },
+            (_result) => {
+                //actually add the friend
+                PlayFabClientAPI.ExecuteCloudScript(
+                    new ExecuteCloudScriptRequest() {
+                        FunctionName = "AcceptFriend",
+                        FunctionParameter = new { ReceiverId = _result.AccountInfo.PlayFabId },
+                    },
+                    (__result) => { Debug.Log(string.Format("Friend request from {0} accepted", _friendName)); },
+                    (__error) => { Debug.LogError(__error.GenerateErrorReport()); }
+                );
+            },
+            (_error) => { Debug.LogError(_error.GenerateErrorReport()); }
+        );
+    }
+
+    public static void DenyFriend(string _friendName) {
+
+    }
+    #endregion
 }
