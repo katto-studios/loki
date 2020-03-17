7f7efb4c60f500a16ea7275d0585e881810d5160
ryanntannn <tanryan777@gmail.com>
2020-03-17 21:09:53 +0800

    Add InventoryManager Script

---
+++ loki-unity/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RenderPlayerKeycaps()
+    {
+        List<ArtisanKeycap> playerKeycaps = PlayfabUserInfo.playerKeycaps;
+        
+        foreach(ArtisanKeycap keycap in playerKeycaps)
+        {
+            
+        }
+    }
+}
