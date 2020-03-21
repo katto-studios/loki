c1cac9c6819222f9546579f4c6054dbc9e27ff9f
ryanntannn <tanryan777@gmail.com>
2020-03-21 12:22:37 +0800

    Add Editor Scripts and Collision Bounds Prefab

---
+++ loki-unity/Assets/Scripts/Inventory/EditorManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorManager : MonoBehaviour
+{
+    public GameObject collisionBoundsPrefab;
+    public Keyboard keyboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!keyboard) keyboard = Keyboard.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
