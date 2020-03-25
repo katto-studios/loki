34bb5b6224740456588d42de87333180b2fc0462
Depressionn <tantiongguan123@gmail.com>
2020-03-25 14:30:57 +0800

    .

---
+++ loki-unity/Assets/Scripts/Multiplayer/RoomInfoDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using System;
+
+public class RoomInfoDisplay : MonoBehaviour {
+    [Header("Fields")]
+    public TextMeshProUGUI roomName;
+    public TextMeshProUGUI roomNumberOfPeople;
+
+    private RoomInfo m_roomInfo;
+    public void SetInfo(RoomInfo _roomInfo) {
+        roomName.SetText(_roomInfo.Name);
+        m_roomInfo = _roomInfo;
+    }
+
+    private IEnumerator UpdateRoomNumberOfPeople() {
+        yield return null;
+    }
+}
