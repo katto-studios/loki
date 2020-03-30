6a68e8a97504d09b557be712c8df5f5239bdcda9
ryanntannn <tanryan777@gmail.com>
2020-03-30 15:46:13 +0800

    Add the bacc button

---
+++ loki-unity/Assets/Scripts/BackButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+    public void GoBackToMainMenu()
+    {
+        GoToScene(1);
+    }
+
+    public void GoToScene(int i)
+    {
+        PersistantCanvas.Instance.ChangeScene(1);
+    }
+}
