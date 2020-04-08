51b78dd5194ba1d70a6d0574b8daf78090c533ca
Depressionn <tantiongguan123@gmail.com>
2020-04-08 19:31:59 +0800

    Added currencies

--- loki-unity/Assets/Scripts/MenuManager.cs
+++ loki-unity/Assets/Scripts/MenuManager.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using PlayFab.ClientModels;
+using PlayFab;
 
 public class MenuManager : MonoBehaviour {
     public TextMeshProUGUI userName;
+    public TextMeshProUGUI scrapMetalDisplay;
+    public TextMeshProUGUI goldDisplay;
 
     // Start is called before the first frame update
     void Start() {
         StartCoroutine(UpdateName());
+        GetCurrency();
 		PlayfabUserInfo.SetUserState(PlayfabUserInfo.UserState.InMainMenu);
     }
 
@@ -20,5 +25,16 @@ public class MenuManager : MonoBehaviour {
             }
             yield return null;
         }
+    }
+
+    private void GetCurrency() {
+        PlayFabClientAPI.GetUserInventory(
+            new GetUserInventoryRequest(),
+            (_result) => {
+                scrapMetalDisplay.SetText(string.Format("Scrap: {0}", _result.VirtualCurrency["SM"]));
+                goldDisplay.SetText(string.Format("Gold: {0}", _result.VirtualCurrency["GC"]));
+            },
+            (_error) => { Debug.LogError(_error); }
+        );
     }
 }
