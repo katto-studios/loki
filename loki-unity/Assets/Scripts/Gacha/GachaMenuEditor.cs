d53fdbff2b954da6f91b717190cf04afdf780326
Depressionn <tantiongguan123@gmail.com>
2020-04-11 04:59:08 +0800

    Very basic gacha

---
+++ loki-unity/Assets/Scripts/Gacha/GachaMenuEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(GachaMenuHandler))]
+public class GachaMenuEditor : Editor {
+    private GachaMenuHandler m_handler;
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        m_handler = (GachaMenuHandler)target;
+
+        if (GUILayout.Button("Gacha")) {
+            m_handler.Gacha();
+        }
+    }
+}
---
+++ loki-unity/Assets/Scripts/Gacha/GachaMenuHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GachaMenuHandler : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Gacha() {
+        KeycapRarity gotThisRarity = GetRate(Random.Range(0.0f, 1.0f));
+        Debug.Log(gotThisRarity.ToString());
+    }
+
+    //I'm following azur lane rates
+    private KeycapRarity GetRate(float _input) {
+        if (_input <= 0.3f) {
+            return KeycapRarity.COMMON;
+        } else if (_input <= 0.81f) {
+            return KeycapRarity.RARE;
+        } else if (_input <= 0.93f) {
+            return KeycapRarity.EPIC;
+        } else {
+            return KeycapRarity.LEGENDARY;
+        }
+    }
+}
