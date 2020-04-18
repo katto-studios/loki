e910649008592e8982c19f6a24756927c0ceb878
Depressionn <tantiongguan123@gmail.com>
2020-04-18 20:26:47 +0800

    Removed a duplicate menu manager

---
+++ loki-unity/Assets/Scripts/Gacha/GachaMenuEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(GachaMenuHandler))]
+public class GachaMenuEditor : Editor {
+    private GachaMenuHandler m_handler;
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        m_handler = (GachaMenuHandler)target;
+        
+    }
+}
---
+++ loki-unity/Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+    public TextMeshProUGUI userName;
+    public RawImage profilePic;
+
+    // Start is called before the first frame update
+    void Start() {
+        UpdateName();
+        UpdateProfilePic();
+		PlayfabUserInfo.SetUserState(PlayfabUserInfo.UserState.InMainMenu);
+    }
+
+    private void UpdateName() {
+        int count = 0;
+        while (true && count ++ < 10000) {
+            userName.text = PlayfabUserInfo.Username;
+            if (!userName.text.Equals("")) {
+                break;
+            }
+        }
+    }
+
+    private void UpdateProfilePic() {
+        int count = 0;
+        while (true && count++ < 10000) {
+            Texture txt = PlayfabUserInfo.ProfilePicture;
+            if (txt) {
+                profilePic.texture = txt;
+                profilePic.color = Color.white;
+                break;
+            }
+        }
+    }
+
+}
