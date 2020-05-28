ca08b860100dab04c699c72e6e280bc6456e656f
Depressionn <tantiongguan123@gmail.com>
2020-05-28 08:25:12 +0800

    Can now get words from playfab servers

--- loki-unity/Assets/Scripts/Multiplayer/TimeTrials/GetWords.cs
+++ loki-unity/Assets/Scripts/Multiplayer/TimeTrials/GetWords.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+using PlayFab;
 using PlayFab.ClientModels;
+using UnityEngine;
 using UnityEngine.Networking;
 using Random = UnityEngine.Random;
 
@@ -9,14 +11,21 @@ using Random = UnityEngine.Random;
 public class GetWords{
     public static event Action eOnInitalised;  
     public static event Action eOnReseted;
+    public static event Action<string> eOnFailure;
     public static string[] Strings{ get; private set; }
     
     public static void Initalise(int _seed){
-        
+        GetText(_seed);
     }
 
-    public static void Inistalise(int _seed, Action _onSuccess, Action _onFailure){
-        
+    public static void Initalise(int _seed, Action _onSuccess, Action<string> _onFailure){
+        eOnInitalised += () => {
+            _onSuccess?.Invoke();
+        };
+        eOnFailure += (_str) => {
+            _onFailure?.Invoke(_str);
+        };
+        GetText(_seed);
     }
 
     public static void Reset(){
@@ -24,14 +33,28 @@ public class GetWords{
         eOnReseted?.Invoke();
     }
 
+    private static void GetText(int _seed){
+        PlayFabClientAPI.GetContentDownloadUrl(
+            new GetContentDownloadUrlRequest(){
+                Key = "TimeTrials/Words.txt"
+            },
+            (_result) => { PersistantCanvas.Instance.StartCoroutine(DownloadText(_result.URL, _seed)); },
+            (_error) => {
+                Debug.LogError(_error.GenerateErrorReport());
+                eOnFailure?.Invoke(_error.GenerateErrorReport());
+            }
+        );
+    }
+
     private static IEnumerator DownloadText(string _url, int _seed){
         using (UnityWebRequest req = UnityWebRequest.Get(_url)){
             yield return req.SendWebRequest();
             if (req.isNetworkError){
                 GameplayConsole.Log("Network error: " + req.error);
+                eOnFailure?.Invoke(req.error);
             }
             else{
-                Strings = RandomizeText(req.downloadHandler.text.Split('\\'), _seed);
+                Strings = RandomizeText(req.downloadHandler.text.Split(new string[]{"\n"}, StringSplitOptions.None), _seed);
                 
                 eOnInitalised?.Invoke();
             }
@@ -40,6 +63,13 @@ public class GetWords{
 
     private static string[] RandomizeText(string[] _randomizeMe, int _seed){
         Random.InitState(_seed);
+        for (int count = 0; count < 100; count++){
+            int number1 = Random.Range(0, _randomizeMe.Length);
+            int number2 = Random.Range(0, _randomizeMe.Length);
+            string temp = _randomizeMe[number1];
+            _randomizeMe[number1] = _randomizeMe[number2];
+            _randomizeMe[number2] = temp;
+        }
         return _randomizeMe;
     }
 }
