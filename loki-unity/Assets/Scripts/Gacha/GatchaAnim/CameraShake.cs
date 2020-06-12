35e230c765912860997147c7daeebc919f978462
Park Yu Seung <ttokong2486@gmail.com>
2020-06-12 18:11:07 +0800

    Updated HDRP cuz VFX Graph refused to work

---
+++ loki-unity/Assets/Scripts/Gacha/GatchaAnim/CameraShake.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Transform camHolder;
+
+    public bool shake;
+    public float shakeDuration;
+    public float shakeMagnitude;
+    public float magnitudeIncrease;
+
+    private float originalShakeMagnitude;
+    private float elapsed = 0.0f;
+    private bool getPos = false;
+    private Vector3 originalPos;
+
+
+    private void LateUpdate()
+    {
+        if (shake)
+        {
+            if (!getPos)
+            {
+                originalPos = camHolder.localPosition;
+                getPos = true;
+                originalShakeMagnitude = shakeMagnitude;
+            }
+
+            if (elapsed < shakeDuration)
+            {
+                float x = Random.Range(-1f, 1f) * shakeMagnitude;
+                float y = Random.Range(-1f, 1f) * shakeMagnitude;
+                camHolder.localPosition = new Vector3(x, y, originalPos.z);
+
+                shakeMagnitude += 0.001f * magnitudeIncrease * Time.deltaTime;
+                elapsed += Time.deltaTime;
+            }
+            else if (elapsed >= shakeDuration)
+            {
+                camHolder.localPosition = originalPos;
+                shakeMagnitude = originalShakeMagnitude;
+                shake = false;
+                getPos = false;
+                elapsed = 0.0f;
+            }
+
+        }
+    }
+
+    public void Shake()
+    {
+        shake = true;
+    }
+
+    public IEnumerator BigShake(float magnitude)
+    {
+        Vector3 ogPos = camHolder.localPosition;
+
+        float e = 0.0f;
+
+        while (e < .2)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y =
+            Random.Range(-1f, 1f) * magnitude;
+            camHolder.localPosition = new Vector3(x, y, ogPos.z);
+
+            e += Time.deltaTime;
+
+            yield return null;
+        }
+        camHolder.localPosition = ogPos;
+    }
+}
---
+++ loki-unity/Assets/Scripts/Gacha/GatchaAnim/GatchaAnim.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatchaAnim : MonoBehaviour
+{
+    public GameObject effect;
+
+    private Animator anim;
+    private Animator rewardAnim;
+    private RaycastHit hit;
+    private Ray ray;
+
+    private bool caseOpened = false;
+
+    public Transform keyCapSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray,out hit))
+        {
+            if (hit.transform.name == "ScreenArea")
+            {
+                anim.SetBool("Idle", false);
+                anim.SetBool("Hover", true);
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    anim.SetBool("Opened", false);
+                    anim.SetBool("Open", true);
+                }
+            }
+            else
+            {
+                anim.SetBool("Idle", true);
+                anim.SetBool("Hover", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Hover", false);
+        }
+
+        if (caseOpened)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                anim.SetBool("Open", false);
+                anim.SetBool("Opened", true);
+                rewardAnim.SetBool("Collected", true);
+                caseOpened = false;
+                effect.SetActive(false);
+            }
+        }
+    }
+
+    public void CaseOpened()
+    {
+        caseOpened = true;
+    }
+
+    public void GatchaReward(GameObject RewardPrefab)
+    {
+        GameObject Reward = Instantiate(RewardPrefab, keyCapSpawn.position, Quaternion.identity) as GameObject;
+        rewardAnim = Reward.GetComponent<Animator>();
+    }
+
+    public void SpawnEffect()
+    {
+        effect.SetActive(true);
+    }
+}
