410e74438809bfa78a52bce6a4dfe66608312a4a
ryanntannn <tanryan777@gmail.com>
2020-06-13 18:28:25 +0800

    CTRL R refresh singleplayer

--- loki-unity/Assets/Scripts/BackButton.cs
+++ loki-unity/Assets/Scripts/BackButton.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackButton : MonoBehaviour
 {
     public bool BackToMainMenu;
+    public bool refresh;
     public void GoBackToMainMenu()
     {
         if (BackToMainMenu)
@@ -32,6 +34,14 @@ public class BackButton : MonoBehaviour
                 scene = PersistantCanvas.Instance.previousScene;
             }
             PersistantCanvas.Instance.ChangeScene(scene);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) && refresh)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                PersistantCanvas.Instance.ChangeScene(SceneManager.GetActiveScene().buildIndex);
+            }
         }
     }
 }
